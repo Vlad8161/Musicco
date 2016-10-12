19607246fb3ae8f61dea46d5138e6d0b30f1c21c
Vlad <vlad81611@yandex.ru>
2016-10-12 22:27:07 +0400

    написал каркас

--- app/src/main/java/mmss/musicco/fragments/AlbumsFragment.java
+++ app/src/main/java/mmss/musicco/fragments/AlbumsFragment.java
@@ -1,10 +1,37 @@
 package mmss.musicco.fragments;
 
-import android.app.ListFragment;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mmss.musicco.R;
 
 /**
  * Created by User on 12.10.2016.
  */
 
-public class AlbumsFragment extends ListFragment {
+public class AlbumsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_albums, null);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("TAG", "AlbumsFragment started");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d("TAG", "AlbumsFragment stopped");
+    }
+
 }
--- app/src/main/java/mmss/musicco/fragments/TracksFragment.java
+++ app/src/main/java/mmss/musicco/fragments/TracksFragment.java
@@ -1,11 +1,37 @@
 package mmss.musicco.fragments;
 
-import android.app.ListFragment;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mmss.musicco.R;
 
 /**
  * Created by User on 12.10.2016.
  */
 
-public class TracksFragment extends ListFragment {
+public class TracksFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tracks, null);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("TAG", "TracksFragment started");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d("TAG", "TracksFragment stopped");
+    }
 
 }
